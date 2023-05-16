fbefe79543c4815f425d8a4af31ce8ca4e5a1a5b
ATHULKRISHNA P P <132829533+athulkrishnapp@users.noreply.github.com>
2023-05-16 15:43:20 +0530

    My Personal Portfolio
    my personal portfolio

---
+++ php/contact.php
@@ -0,0 +1,10 @@
+<?php
+
+$name= $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to = "athulkrishnapp5833@gmail.com";
+$txt=" Name =". $name ."\r\n Email =". $email ."\r\n Mobile Number =". $phone ."\r\n Subject =". $subject ."\r\n Message=". $message ;
+?>
